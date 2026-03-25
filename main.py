6a2ddb10fbba588ee4f5aca53f26fbc395af3eb5
Mitansh05 <mitansh341@gmail.com>
2026-03-25 19:34:55 -0400

    First Commit
    Added all the essential files and a basic menu

---
+++ main.py
@@ -0,0 +1,15 @@
+## Program - AirWatch ##
+
+## Disclaimer: This program might require to be runned twice for the menu to work in the terminal. ##
+
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+print("\nActivating Program - AirWatch\n")
+
+print("Select an option:\n 1. Via CallSign\n 2. See Total Count of Aircrafts Via Country\n 3. Exit")
+input1 = input("\nEnter your choice: ")
+
+if input1 == "1":
+    callsign = input("\nEnter the CallSign of the Aircraft: ")
